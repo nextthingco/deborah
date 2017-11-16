ba7ece2394c5a30bd242849e984137732703c748
Alex Kaplan <kaplan2539@gmail.com>
2017-11-15 22:44:59 -0800

    k_build.sh: debug

--- k_build.sh
+++ k_build.sh
@@ -6,7 +6,7 @@ chmod 0600 "${LINUX_DPK_FILE}"
 
 export GIT_SSH_COMMAND="ssh -i ${LINUX_DPK_FILE} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
-ls -l "${LINUX_DPK_FILE}
+ls -l "${LINUX_DPK_FILE}"
 cat ${LINUX_DPK_FILE}
 
 LINUX_BRANCH=${LINUX_BRANCH:-chip4-4.13.y}
